4dfd7e13d8da141155039601bfd5ec0bb71421d4
KC Barrett <kbarre123@gmail.com>
2014-06-25 14:45:32 -0500

    Finished

---
+++ 01_Analysis_of_Algorithms/QuickFind/src/quickfind/QuickFindTester.java
@@ -0,0 +1,33 @@
+/**
+ * Client program to test implementation of QuickFind.java.
+ */
+
+package quickfind;
+import edu.princeton.cs.introcs.*;
+
+/**
+ * @author kcb
+ */
+public class QuickFindTester 
+{   
+    /**
+     * Reads in a sequence of pairs of integers (between 0 and N-1) from standard input, 
+     * where each integer represents some object;
+     * if the objects are in different components, merge the two components
+     * and print the pair to standard output.
+     * @param args
+     */
+    public static void main(String[] args) {
+        int N = StdIn.readInt();
+        QuickFind uf = new QuickFind(N);
+        while (!StdIn.isEmpty()) {
+            int p = StdIn.readInt();
+            int q = StdIn.readInt();
+            if (uf.connected(p, q)) continue;
+            uf.union(p, q);
+            StdOut.println(p + " " + q);
+        }
+        // Once done entering pairs, hit ctrl-D to indicate EOL & skip to this
+        StdOut.println(uf.count() + " components");
+    }
+}
